b66f4c817773f91b69bbe99e8c3b472c7d6ffbf4
Georg Makowski <gm@desider.at>
2023-05-17 12:47:06 +0200

    refactor: module imports and config filename

--- _vendor/github.com/bowman2001/hugo-mod-mermaid/assets/js/mermaid.js
+++ _vendor/github.com/bowman2001/hugo-mod-mermaid/assets/js/mermaid.js
@@ -1,2 +1,2 @@
-import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@{{ site.Params.mermaid.version }}/dist/mermaid.esm.min.mjs';
-mermaid.initialize({ startOnLoad: true, 'theme': '{{ .Params.mermaid.theme | default site.Params.mermaid.theme }}' });
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@{{ or $.Params.mermaid.version site.Data.hugoModMermaid.version "latest" }}/dist/mermaid.esm.min.mjs';
+mermaid.initialize({ startOnLoad: true, 'theme': '{{ or $.Params.mermaid.theme site.Data.hugoModMermaid.theme "base" }}' });
