0ffbd6db39f9d689f289911904c21cce2e297c3c
Georg Makowski <gm@desider.at>
2023-05-19 19:59:45 +0200

    chore: module updates

--- _vendor/github.com/bowman2001/hugo-mod-mermaid/assets/js/mermaid.js
+++ _vendor/github.com/bowman2001/hugo-mod-mermaid/assets/js/mermaid.js
@@ -1,2 +1,9 @@
-import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@{{ or $.Params.mermaid.version site.Data.hugoModMermaid.version "latest" }}/dist/mermaid.esm.min.mjs';
-mermaid.initialize({ startOnLoad: true, 'theme': '{{ or $.Params.mermaid.theme site.Data.hugoModMermaid.theme "base" }}' });
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@{{ $.Params.mermaid.version | default "latest" }}/dist/mermaid.esm.min.mjs';
+{{ $config := slice }}
+{{ with site.Data.hugoModMermaid }}
+    {{- range $k, $v := . -}}
+        {{- $config = $config | append (printf "%q: %q" $k $v) }}
+    {{- end }}
+    {{ $config = delimit $config ", " }}
+{{- end }}
+mermaid.initialize({ startOnLoad: true {{ with $config }}, {{ . }}{{ end }} });
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/bowman2001/perplex
 go 1.20
 
 require (
-	github.com/bowman2001/hugo-mod-katex v0.2.1 // indirect
-	github.com/bowman2001/hugo-mod-mermaid v0.2.0 // indirect
+	github.com/bowman2001/hugo-mod-katex v0.2.2 // indirect
+	github.com/bowman2001/hugo-mod-mermaid v0.3.0 // indirect
 	github.com/bowman2001/hugo-mod-meta v0.1.1 // indirect
 	github.com/bowman2001/hugo-mod-open-in-vscode v1.2.0 // indirect
 	github.com/bowman2001/hugo-mod-replacements v0.3.2 // indirect
