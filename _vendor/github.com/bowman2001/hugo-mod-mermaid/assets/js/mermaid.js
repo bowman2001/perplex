c7603346a2c1772c971d1a7d06dee7a35a04f49b
Georg Makowski <gm@desider.at>
2023-07-04 14:15:22 +0200

    feat: pagefind

--- _vendor/github.com/bowman2001/hugo-mod-mermaid/assets/js/mermaid.js
+++
@@ -1,9 +0,0 @@
-import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@{{ $.Params.mermaid.version | default "latest" }}/dist/mermaid.esm.min.mjs';
-{{ $config := slice }}
-{{ with site.Data.hugoModMermaid.config }}
-    {{- range $k, $v := . -}}
-        {{- $config = $config | append (printf "%q: %q" $k $v) }}
-    {{- end }}
-    {{ $config = delimit $config ", " }}
-{{- end }}
-mermaid.initialize({ startOnLoad: true {{ with $config }}, {{ . }}{{ end }} });
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.20
 
 require (
 	github.com/bowman2001/hugo-mod-katex v0.2.3 // indirect
-	github.com/bowman2001/hugo-mod-mermaid v0.4.0 // indirect
+	github.com/bowman2001/hugo-mod-mermaid v0.4.1 // indirect
 	github.com/bowman2001/hugo-mod-meta v0.2.0 // indirect
 	github.com/bowman2001/hugo-mod-open-in-vscode v1.2.0 // indirect
 	github.com/bowman2001/hugo-mod-replacements v0.3.2 // indirect
