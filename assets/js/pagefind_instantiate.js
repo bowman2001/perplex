94a65e099f3a4f3a692bf02dd2e2edeaaf8f653c
Georg Makowski <gm@desider.at>
2023-07-28 02:14:19 +0200

    fix: add baseUrl path to pagefind instance

--- assets/js/pagefind_instantiate.js
+++ assets/js/pagefind_instantiate.js
@@ -1,6 +1,8 @@
+{{- $u := urls.Parse site.BaseURL -}}
 window.addEventListener('DOMContentLoaded', (event) => {
     new PagefindUI({
         "element": "#search-with-pagefind",
         "showImages": true,
+        "baseUrl": "{{ $u.Path }}",
     });
 });
