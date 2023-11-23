802bc1c3be1cd29debb1f6192f0eb7973f786385
Georg Makowski <gm@desider.at>
2023-11-23 10:23:56 +0100

    chore: bump mod img

--- _vendor/github.com/bowman2001/hugo-mod-image/assets/mod-img/js/ls.config.js
+++ _vendor/github.com/bowman2001/hugo-mod-image/assets/mod-img/js/ls.config.js
@@ -1,12 +1,10 @@
 window.lazySizesConfig = window.lazySizesConfig || {};
 window.lazySizesConfig.getOptimumX = function(_element){
-    var dpr = window.devicePixelRatio || 1;
-	if(dpr > 2.6){
-		dpr *= 0.6666; 
-	} else if(dpr > 1.9){
-		dpr *= 0.85; 
+    let dpr = window.devicePixelRatio || 1;
+	if ( dpr > 1 ) {
+		dpr = Math.log( dpr ) + 0.99;
 	} else {
 		dpr -= 0.01;
-	}
-	return Math.min(Math.round(dpr * 100) / 100, 2);
+	}	
+	return Math.min(Math.round(dpr * 100) / 100, 2.61);
 };
