81471393f9fda17e8eb796744a71c1a0038603d2
Georg Makowski <gm@desider.at>
2024-02-26 11:34:38 +0100

    fix: code block copy button in lists
    There is no right padding for code blocks embedded in lists. The button doesn’t need any offset, then.

--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -3,7 +3,7 @@ document.querySelectorAll('pre.chroma > code').forEach((codeBlock) => {
     // tr only may be table row in case of line numbers in table
     const tr = codeBlock.parentNode.parentNode.parentNode;
 
-    if( tr.firstChild.firstChild.firstChild === codeBlock) {
+    if( tr.nodeName === 'TR' && tr.firstChild.firstChild.firstChild === codeBlock) {
         // Do nothing because this code block contains only line numbers
         return
     }
@@ -12,6 +12,7 @@ document.querySelectorAll('pre.chroma > code').forEach((codeBlock) => {
     const copyButton = document.createElement('button');
     copyButton.classList.add('copy-code');
     copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5.938 14.896V1.375h11.02v13.521Zm-3.73 3.729V5.188h1.98v11.458h8.958v1.979Z"/></svg>';
+
 
     function copyingDone() {
         copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5.917 14.917V1.438h10.979v13.479Zm1.979-1.979h7.021V3.417H7.896Zm-5.625-.605H4.25v-1.75H2.271Zm0-3H4.25v-1.75H2.271ZM8.5 18.562h1.75v-1.979H8.5Zm-6.229-3.229H4.25v-1.75H2.271Zm0 3.229H4.25v-1.979H2.271Zm3.229 0h1.75v-1.979H5.5Zm6 0h1.979v-1.979H11.5ZM2.271 6.333H4.25V4.354H2.271Z"/></svg>';
